6f38928dce3a402d1db9693ae9c6f8e0c10a718a
Fahri Izzuddin Zulkarnaen <fahrizulkarnaen1@gmail.com>
2022-07-15 11:24:50 +0700

    add link cek bed rumah sakit

--- src/covid-mapping-indonesia/containers/Footer/Footer.jsx
+++ src/covid-mapping-indonesia/containers/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import { Row, Col } from "react-bootstrap";
 import styles from "./Footer.module.css";
 
 class Footer extends Component {
     render() {
         return (
             <div id={styles["footer"]}>
-                <div>
+                <div className={styles["footer-content-container"]}>
                     <p className={styles["footer-desc"]}>
                         Design and Developed by{" "}
                         <span
@@ -16,12 +17,30 @@ class Footer extends Component {
                             SpaceCapt-Tech @covid-2022
                         </span>{" "}
                     </p>
-                    <p className={styles["footer-desc"]}>
-                        Using API From
-                        <a className={styles["footer-link"]} href="https://covid19.go.id/dokumentasi-api">
-                            Covid-19.go.id
-                        </a>
-                    </p>
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <p className={styles["footer-desc"]}>
+                            Using API From
+                            <a className={styles["footer-link"]} href="https://covid19.go.id/dokumentasi-api">
+                                Covid-19.go.id
+                            </a>
+                        </p>
+                        <p
+                            className={styles["footer-desc"]}
+                            style={{
+                                marginLeft: "20px",
+                            }}
+                        >
+                            Cek Ketersediaan Bed Rumah Sakit
+                            <a className={styles["footer-link"]} href="https://cek-bed-rs-indonesia.herokuapp.com/">
+                                cek-bed-rs-indonesia.com
+                            </a>
+                        </p>
+                    </div>
                 </div>
             </div>
         );
