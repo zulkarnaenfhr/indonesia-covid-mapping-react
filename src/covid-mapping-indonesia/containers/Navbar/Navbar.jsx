5930dc57671b00f0f5b23870d2ea2f7058fe1b9e
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-07-19 09:15:11 +0700

    add navbar responsive

--- src/covid-mapping-indonesia/containers/Navbar/Navbar.jsx
+++ src/covid-mapping-indonesia/containers/Navbar/Navbar.jsx
@@ -11,38 +11,44 @@ const Navbar = (props) => {
         <div id={styles["Navbar"]}>
             <Container fluid>
                 <Row>
-                    <Col lg={5}>
+                    <Col md={5}>
                         <h1 className={styles["Navbar-Branding"]}>
                             <span>Covid-19 ‎</span> Indonesia Live Mapping
                         </h1>
                     </Col>
-                    <Col lg={7} className={styles["Navbar-Column-Right"]}>
-                        <div className={styles["Navbar-Form-Box"]}>
-                            <h1 className={styles["Navbar-Form-Box-Title"]}>Update Date</h1>
-                            <h2 className={styles["Navbar-Form-Box-Desc"]}>{props.statusLoad ? props.tanggalUpdate : <Spinner animation="grow" size="sm" />}</h2>
-                        </div>
-                        <div className={styles["Navbar-Form-Box"]}>
-                            {props.statusLoad ? (
-                                <form action="">
-                                    <h1 className={styles["Navbar-Form-Box-Title"]}>Province</h1>
-                                    <select onChange={handleOnChange} className={styles["selectOption"]}>
-                                        <option value="">Select Province</option>
-                                        {props.province.map((prov) => {
-                                            return (
-                                                <option key={prov} value={prov}>
-                                                    {prov}
-                                                </option>
-                                            );
-                                        })}
-                                    </select>
-                                </form>
-                            ) : (
-                                <>
-                                    <h1 className={styles["Navbar-Form-Box-Title"]}>Province</h1>
-                                    <Spinner animation="grow" size="sm" />
-                                </>
-                            )}
-                        </div>
+                    <Col md={7} className={styles["Navbar-Column-Right"]}>
+                        <Row className={styles["Navbar-Column-Right-Container"]}>
+                            <Col className={styles["Navbar-Column-Right-Container-Column"]}>
+                                <div className={styles["Navbar-Form-Box"]}>
+                                    <h1 className={styles["Navbar-Form-Box-Title"]}>Update Date</h1>
+                                    <h2 className={styles["Navbar-Form-Box-Desc"]}>{props.statusLoad ? props.tanggalUpdate : <Spinner animation="grow" size="sm" />}</h2>
+                                </div>
+                            </Col>
+                            <Col className={styles["Navbar-Column-Right-Container-Column"]}>
+                                <div className={styles["Navbar-Form-Box"]}>
+                                    {props.statusLoad ? (
+                                        <form action="">
+                                            <h1 className={styles["Navbar-Form-Box-Title"]}>Province</h1>
+                                            <select onChange={handleOnChange} className={styles["selectOption"]}>
+                                                <option value="">Select Province</option>
+                                                {props.province.map((prov) => {
+                                                    return (
+                                                        <option key={prov} value={prov}>
+                                                            {prov}
+                                                        </option>
+                                                    );
+                                                })}
+                                            </select>
+                                        </form>
+                                    ) : (
+                                        <>
+                                            <h1 className={styles["Navbar-Form-Box-Title"]}>Province</h1>
+                                            <Spinner animation="grow" size="sm" />
+                                        </>
+                                    )}
+                                </div>
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </Container>
